1ec99a6f8f953fc79e932788a75afc66a388456d
Gilles RUSZCZYCKI <gilles.ruszczycki@ck.lu>
2025-12-02 11:45:05 +0100

    feat: login page + protected route working

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,49 @@
+
+import { useState } from "react";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/auth/login", { email, password });
+
+      localStorage.setItem("token", res.data.token);
+
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Email ou mot de passe incorrect");
+    }
+  };
+
   return (
     <div>
-      <h1>Page de Login</h1>
+      <h1>Connexion</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Se connecter</button>
+      </form>
     </div>
   );
 }
