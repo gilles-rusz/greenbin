9f2836b75ca11bb590b8119d05d8b4970e39d364
Gilles RUSZCZYCKI <gilles.ruszczycki@ck.lu>
2025-12-02 14:36:24 +0100

    feat:wastelist page + wasteservice

--- frontend/src/pages/WastesList.jsx
+++ frontend/src/pages/WastesList.jsx
@@ -1,5 +1,38 @@
-export default function Dashboard() {
+
+import { useEffect, useState } from "react";
+import { getAllWastes } from "../services/wasteService.js";
+
+export default function WastesList() {
+  const [wastes, setWastes] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getAllWastes();
+        setWastes(data);
+      } catch (err) {
+        console.error("Erreur récupération déchets", err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <h1>Dashboard</h1>
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Liste des déchets</h1>
+
+      {wastes.length === 0 ? (
+        <p>Aucun déchet trouvé.</p>
+      ) : (
+        <ul>
+          {wastes.map((w) => (
+            <li key={w.id}>
+              <strong>{w.name}</strong> - {w.category}  
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
