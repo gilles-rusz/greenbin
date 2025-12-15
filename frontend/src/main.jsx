5bcc3da1f3228784c550b26059c851ae402cab58
Gilles RUSZCZYCKI <gilles.ruszczycki@ck.lu>
2025-12-15 11:47:39 +0100

    Fix user update route and align front-end URLs with REST conventions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import "./style/variables-et-reset.css";
 import "./style/global-layout.css";
